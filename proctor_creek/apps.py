66015c360119f2905379dd980fbf9171b773ca22
dylan <dsmall6@gatech.edu>
2021-04-12 19:28:07 -0400

    setup proctor_creek basic app; created templates and static functionality

---
+++ proctor_creek/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProctorCreekConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'proctor_creek'
--- proctor_creek/urls.py
+++ proctor_creek/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import IndexView
 
 urlpatterns = [
 
-    path('', include('proctor_creek.urls')),
+    path('', IndexView.as_view(), name='index'),
 ]
